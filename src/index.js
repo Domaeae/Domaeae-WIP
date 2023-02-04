a8518d225c9437a1fb9a2ad48bc4e2aca5715785
Domaeae <DominikGirrbach@live.de>
2023-02-04 10:21:09 +0100

    +1

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import GoTrue from 'gotrue-js';
-
-// Instantiate the GoTrue auth client with an optional configuration
-
-auth = new GoTrue({
-    APIUrl: 'https://domaeae-wip.netlify.app/.netlify/identity',
-    audience: '',
-    setCookie: false,
-});
